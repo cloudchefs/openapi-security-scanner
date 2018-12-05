883eb186389fab35800de702de97e2ba207435c0
Ash Belmokadem <a.belmokadem@gmail.com>
2018-12-05 08:15:23 +0100

    Add test case for request generator

--- packages/request-generator/test/index.js
+++ packages/request-generator/test/index.js
@@ -1,56 +1,18 @@
-const snapshot = require('../lib/snapshot')
 const tape = require('tape')
-const xmlClient = require('../lib/xml-client')
-const jsonClient = require('../lib/json-client')
+const requestGenerator = require('../lib')
 
 tape.test(
-  '[json-client] /get/report: invalid hash should return an error',
-  async t => {
-    try {
-      await jsonClient.getReport({ hash: 'INVALID_HASH' })
-    } catch (error) {
-      t.equals(error.name, 'StatusCodeError')
-      t.equals(error.statusCode, 421)
+    'request-generator should generate test collection per path, method, schemes combination',
+    async t => {
+        const apiDefinition = require('./fixtures/api')
+
+        try {
+            const requests = await requestGenerator(apiDefinition)
+            t.equals(requests.length, 4)
+        } catch (error) {
+            t.fail()
+        }
+
+        t.end()
     }
-
-    t.end()
-  }
-)
-
-tape.test(
-  '[xml-client] /get/report: invalid hash should return an error',
-  async t => {
-    try {
-      await xmlClient.getReport({ hash: 'INVALID_HASH' })
-    } catch (error) {
-      t.equals(error.name, 'StatusCodeError')
-      t.equals(error.statusCode, 421)
-    }
-
-    t.end()
-  }
-)
-
-tape.test(
-  '[json-client] /get/report: valid hash should return a proper response',
-  async t => {
-    const actual = await jsonClient.getReport({
-      hash: 'afe6b95ad95bc689c356f34ec8d9094c495e4af57c932ac413b65ef132063acc'
-    })
-
-    snapshot(t, actual, 'get/report/response.json')
-    t.end()
-  }
-)
-
-tape.test(
-  '[xml-client] /get/report: valid hash should return a proper response',
-  async t => {
-    const actual = await xmlClient.getReport({
-      hash: 'afe6b95ad95bc689c356f34ec8d9094c495e4af57c932ac413b65ef132063acc'
-    })
-
-    snapshot(t, actual, 'get/report/response.xml')
-    t.end()
-  }
 )
